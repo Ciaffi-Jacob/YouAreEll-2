bcb2929bcdad9371765e699b0dffa0a3bcd4e048
Miamivice2 <andreb.davis2@gmail.com>
2023-04-09 21:46:12 -0400

    a little confused going to commit

--- Client/src/main/java/controllers/ServerController.java
+++ Client/src/main/java/controllers/ServerController.java
@@ -1,18 +1,20 @@
 package controllers;
 
-//import spiffyURLManipulator
+//import spiffyUrlManipulator
+
 import models.Id;
 
 import javax.json.JsonString;
 import java.net.HttpURLConnection;
 
-public class ServerController{
-  // HttpURLConnection
-    private String rootURL = "http://zipcode.rocks:8085";
+public class ServerController {
+    private String rootURL = "http://zipcode.rocks:8085/ids";
 
     private ServerController svr = new ServerController();
 
-    private ServerController() {}
+    private ServerController() {
+
+    }
 
     public ServerController shared() {
         return svr;
@@ -24,7 +26,7 @@ public class ServerController{
         // return json from server
         return null;
     }
-    public JsonString idPost(Id id) {
+    public JsonString idPost(Id input) {
         // url -> /ids/
         // create json from Id
         // request
@@ -32,12 +34,13 @@ public class ServerController{
         // return json
         return null;
     }
-    public JsonString idPut(Id id) {
+    public JsonString idPut(Id input) {
         // url -> /ids/
         return null;
     }
 
 
 }
+
 
 // ServerController.shared.doGet()
--- Client/src/main/java/controllers/TransactionController.java
+++ Client/src/main/java/controllers/TransactionController.java
@@ -1,27 +1,76 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import models.Id;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionController {
     private String rootURL = "http://zipcode.rocks:8085";
     private MessageController msgCtrl;
     private IdController idCtrl;
+    List<JSONPObject> user = new ArrayList<>();
+    List<JSONPObject> messages = new ArrayList<>();
+    private StringBuilder informationStringUsers;
+    private StringBuilder informationStringMessages;
+    StringBuilder informationString;
+    URL urlID = new URL(rootURL + "/ids");
+    URL urlMessages = new URL("http://xipcode.rocks:8085/massages");
 
-    public TransactionController(MessageController m, IdController j) {}
+    {
 
-    public List<Id> getIds() {
+        try {
+            URL url = urlID;
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                conn.setRequestMethod("GET");
+            } catch (ProtocolException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                conn.connect();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-        return null;
-    }
-    public String postId(String idtoRegister, String githubName) {
-        Id tid = new Id(idtoRegister, githubName);
-        tid = idCtrl.postId(tid);
-        return ("Id registered.");
-    }
+            int responseCode = 0;
+            try {
+                responseCode = conn.getResponseCode();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-    public String makecall(String s, String get, String s1) {
-        return null;
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode:")
+            }
+
+
+    public TransactionController(MessageController m, IdController j) {
+            }
+
+            public List<Id> getIds () {
+
+                return null;
+            }
+            public String postId (String idtoRegister, String githubName){
+                Id tid = new Id(idtoRegister, githubName);
+                tid = idCtrl.postId(tid);
+                return ("Id registered.");
+            }
+
+            public String makecall (String s, String get, String s1){
+                return null;
+            }
+        }
     }
 }
